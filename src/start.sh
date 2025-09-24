47b02d38b7f55416b2e83a0048d655b790002dc5
yubiuser <github@yubiuser.dev>
2025-09-24 10:04:13 +0200

    Use FTL's build-in wait-for command for checking the logs
    Signed-off-by: yubiuser <github@yubiuser.dev>

--- src/start.sh
+++ src/start.sh
@@ -75,10 +75,9 @@ start() {
         sleep 0.5
     done
 
-    #  Wait until the FTL log contains the "FTL started" message before continuing
-    while ! grep -q '########## FTL started' /var/log/pihole/FTL.log; do
-        sleep 0.5
-    done
+    #  Wait until the FTL log contains the "FTL started" message before continuing, timeout after 10 seconds
+    logsize_before=$(stat -c%s /var/log/pihole/FTL.log)
+    /usr/bin/pihole-FTL wait-for '########## FTL started' /var/log/pihole/FTL.log 10 "${logsize_before}"
 
     pihole updatechecker
     local versionsOutput
